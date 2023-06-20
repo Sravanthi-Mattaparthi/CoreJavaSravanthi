b56b0af0cdc3082a114cf9a2d530e956c83d3035
Mattaparthi Lakshmi Sravanthi <m.lakshmisravanthi@gmail.com>
2023-06-20 21:49:32 +0530

    S1

---
+++ Java03_Literals/src/com/dl/one/Eg1.java
@@ -0,0 +1,35 @@
+package com.dl.one;
+
+public class Eg1 {
+
+	public static void main(String[] args) {
+	 //Literals
+	//Integer literals
+		int a= 10;
+		System.out.println(a);
+		
+		float f1 =20.f;
+		System.out.println(f1);
+		
+		double d1=30.0d;
+		System.out.println(d1);
+		
+		boolean b= true;
+		System.out.println(b);
+		
+		char ch1= 'A';
+		System.out.println(ch1);
+		
+		char ch2= 97;
+		System.out.println(ch2);
+		
+		char ch3= '\u0000';
+		System.out.println(ch3);
+			
+	}
+
+}
+
+//KeyPoint
+
+// Refer the ASCI codes for character implemented with ch2=97, ch3=\00003
---
+++ Java03_Literals/src/com/dl/one/Eg2.java
@@ -0,0 +1,23 @@
+package com.dl.one;
+
+public class Eg2 {
+
+	public static void main(String[] args) {
+		// binary,octal,hexa
+		
+		//binary base2 , prefix 0b, (0 and 1)
+		int i = 0b0011;
+		System.out.println(i);
+		
+		//octal base8 , prefix 0, (0 and 7)
+				int j = 0123;
+				System.out.println(j);
+				
+	    //hexa base2 , prefix 0x, (a to f and , 0 to 9)
+				int k= 0xace;
+				System.out.println(k);
+			
+
+	}
+
+}
